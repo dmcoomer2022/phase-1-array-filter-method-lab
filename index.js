27348824488ba5fc86023da4c74cf9145bb51b0a
David Coomer <dmcoomer@hotmail.com>
2022-04-02 23:38:42 -0400

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(driversArray, preacherPaul) {
+return driversArray.filter(element => element.toLowerCase() === preacherPaul.toLowerCase());
+} 
+
+function fuzzyMatch(driversArray, prickleyBear) {
+    return driversArray.filter(element => element[0]===prickleyBear[0])
+
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(element => element.name === name)
+}
